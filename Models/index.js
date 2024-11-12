f713d97ddc8adacc0622d4e53f08b8bee043895e
AFreborn22 <aafauzan52@gmail.com>
2024-11-13 00:18:48 +0700

    yaaa

--- Models/index.js
+++ Models/index.js
@@ -3,10 +3,9 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const config = require(path.resolve(__dirname, '../../config/config.js'))[env];
 const db = {};
 
 let sequelize;
@@ -27,38 +26,7 @@ fs
     );
   })
   .forEach(file => {
-    try {
-      const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-      db[model.name] = model;
-    } catch (error) {
-      console.error(`Error loading model: ${file}`, error);
-    }
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    try {
-      db[modelName].associate(db);
-    } catch (error) {
-      console.error(`Error associating model: ${modelName}`, error);
-    }
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = require(path.join(__dirname, file));
     db[model.name] = model;
   });
 
