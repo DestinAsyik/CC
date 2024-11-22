73f1011cdccc7a9cd3489d653d23d170ff3f2d4e
AFreborn22 <aafauzan52@gmail.com>
2024-11-22 21:35:27 +0700

    yapss

--- Routes/reccom.js
+++ Routes/reccom.js
@@ -3,7 +3,7 @@ const reccomController = require('../Controllers/reccomenderController')
 const router = express.Router()
 
 router.post('/reccomendation/content', reccomController.reccomByContent);
-router.post('/reccomendation/distance', reccomController.reccomByJarak);
+router.post('/reccomendation/nearby', reccomController.reccomByJarak);
 router.post('/reccomendation/review', reccomController.reccomHybrid);
 
 module.exports = router;
