d75831aa8cd50dd86993b6433f1d7bbc85fa691d
indrioolivia <indrioliviasafitri@gmail.com>
2024-11-26 11:31:51 +0700

    chore(docker): add dockerfile and docker-compose configurations

--- config/config.js
+++ config/config.js
@@ -1,22 +1,33 @@
 require('dotenv').config();
 const mysql2 = require('mysql2');
 
-module.exports = {
+const config = {
   development: {
-    username: 'root',
-    password: 'strongpassword',
-    database: 'destinasyik',
-    host: '127.0.0.1',
-    port: 3306,
-    dialect: 'mysql'
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || 'strongpassword',
+    database: process.env.DB_NAME || 'destinasyik',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+    dialectModule: mysql2,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    dialectOptions: {
+      connectTimeout: 60000
+    }
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'destinasyik',
-    host: '127.0.0.1',
-    port: 3306,
-    dialect: 'mysql'
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    dialectModule: mysql2
   },
   production: {
     username: process.env.DB_USERNAME,
@@ -26,5 +37,19 @@ module.exports = {
     port: process.env.DB_PORT,
     dialect: 'mysql',
     dialectModule: mysql2,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    dialectOptions: {
+      connectTimeout: 60000,
+      ssl: {
+        rejectUnauthorized: true
+      }
+    }
   }
 };
+
+module.exports = config;
