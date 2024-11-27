dd52722d1c6eb94e81d80cbb92b63cad6be6d504
AFreborn22 <aafauzan52@gmail.com>
2024-11-27 17:53:20 +0700

    sync migrations

--- Routes/reccom.js
+++ Routes/reccom.js
@@ -2,8 +2,8 @@ const express = require('express')
 const reccomController = require('../Controllers/reccomenderController')
 const router = express.Router()
 
-router.post('/reccomendation/content', reccomController.reccomByContent);
-router.post('/reccomendation/nearby', reccomController.reccomByJarak);
-router.post('/reccomendation/colaborative', reccomController.reccomHybrid);
+router.post('/content', reccomController.reccomByContent);
+router.post('/nearby', reccomController.reccomByJarak);
+router.post('/colaborative', reccomController.reccomHybrid);
 
 module.exports = router;
