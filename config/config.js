b9310343e0e00699b074cfce18eb25949d1b2e9b
AFreborn22 <aafauzan52@gmail.com>
2024-11-27 16:10:48 +0700

    setup helpers for secret manager

--- config/config.js
+++ config/config.js
@@ -21,11 +21,11 @@ module.exports = {
     logging: false, 
   },
   production: {
-    username: process.env.DB_USERNAME = await accessSecret('DB_USERNAME'),
-    password: process.env.DB_PASSWORD = await accessSecret('DB_PASSWORD'),
-    database: process.env.DB_NAME = await accessSecret('DB_NAME'),
-    host: process.env.DB_HOST = await accessSecret('DB_HOST'),
-    port: process.env.DB_PORT = await accessSecret('DB_PORT'),
+    username: process.env.DB_USERNAME, //= await accessSecret('DB_USERNAME'),
+    password: process.env.DB_PASSWORD, //= await accessSecret('DB_PASSWORD'),
+    database: process.env.DB_NAME, //= await accessSecret('DB_NAME'),
+    host: process.env.DB_HOST, //= await accessSecret('DB_HOST'),
+    port: process.env.DB_PORT, //= await accessSecret('DB_PORT'),
     dialect: 'mysql',
     logging: false, 
     dialectOptions: {
